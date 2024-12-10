30e7345b5da954b68b2f00fa4a92c618e2827b01
simon <simonescuela9@gmail.com>
2024-12-10 13:44:18 +0000

    modificado con las ForeignKey, entidad relacion

--- app/src/main/java/com/simon/proyectomapasimon/datos/Marcador.kt
+++ app/src/main/java/com/simon/proyectomapasimon/datos/Marcador.kt
@@ -1,11 +1,24 @@
 package com.simon.proyectomapasimon.datos
 
-import androidx.room.*
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
 
-@Entity(tableName = "marcadores")
+@Entity(
+    tableName = "marcadores",
+    foreignKeys = [ForeignKey(
+        entity = TipoMarcador::class,
+        parentColumns = ["id"],
+        childColumns = ["tipoId"],
+        onDelete = ForeignKey.CASCADE
+    )],
+    indices = [Index("tipoId")]
+)
 data class Marcador(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val latitud: Double,
     val longitud: Double,
-    val titulo: String
+    val titulo: String,
+    val tipoId: Int
 )
