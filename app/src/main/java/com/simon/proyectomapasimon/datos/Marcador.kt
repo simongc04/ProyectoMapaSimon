dd4ed3882f725f94dd43a903b5c34aceb50a4a03
simon <simonescuela9@gmail.com>
2024-12-10 13:30:23 +0000

    realizado

--- app/src/main/java/com/simon/proyectomapasimon/datos/Marcador.kt
+++ app/src/main/java/com/simon/proyectomapasimon/datos/Marcador.kt
@@ -1,2 +1,11 @@
 package com.simon.proyectomapasimon.datos
 
+import androidx.room.*
+
+@Entity(tableName = "marcadores")
+data class Marcador(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val latitud: Double,
+    val longitud: Double,
+    val titulo: String
+)
