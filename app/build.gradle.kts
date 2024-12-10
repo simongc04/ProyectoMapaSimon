c0e97e9e998e66f9b7dc2d6832105020ba8bd73d
simon <simonescuela9@gmail.com>
2024-12-10 13:30:05 +0000

    añadido

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
+
+
+
 }
 
 android {
@@ -40,6 +44,15 @@ android {
 }
 
 dependencies {
+
+    implementation (libs.androidx.room.runtime)
+    ksp (libs.androidx.room.compiler)
+
+    implementation (libs.androidx.room.ktx)
+
+
+    implementation (libs.androidx.core.ktx.v1120)
+    implementation (libs.kotlin.stdlib)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
