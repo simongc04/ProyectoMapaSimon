8412dc769e882159d92395b3427666ccd0a260c8
simon <simonescuela9@gmail.com>
2024-12-10 13:45:25 +0000

    Realizado

--- app/src/main/java/com/simon/proyectomapasimon/datos/MarcadorDao.kt
+++ app/src/main/java/com/simon/proyectomapasimon/datos/MarcadorDao.kt
@@ -1,4 +1,21 @@
 package com.simon.proyectomapasimon.datos
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
 interface MarcadorDao {
+    @Query("SELECT * FROM marcadores")
+    fun getAllMarcadores(): List<Marcador>
+
+    @Query("SELECT * FROM tipo_marcadores")
+    fun getAllTipos(): List<TipoMarcador>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertMarcador(marcador: Marcador)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertTipoMarcador(tipo: TipoMarcador)
 }
