1973efd698e1d077518f43f5b700c46f9b42f6ed
simon <simonescuela9@gmail.com>
2024-12-10 13:43:23 +0000

    realizado

--- app/src/main/java/com/simon/proyectomapasimon/datos/TipoMarcador.kt
+++ app/src/main/java/com/simon/proyectomapasimon/datos/TipoMarcador.kt
@@ -1,3 +1,10 @@
 package com.simon.proyectomapasimon.datos
 
-data class TipoMarcador()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tipo_marcadores")
+data class TipoMarcador(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val nombre: String
+)
